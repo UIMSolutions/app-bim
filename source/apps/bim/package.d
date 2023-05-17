e3031629af8bd6e718aae0f52f147c1bbaaff4e5
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-17 11:33:01 +0200

    Update

--- source/apps/bim/package.d
+++ source/apps/bim/package.d
@@ -2,13 +2,33 @@ module apps.bim;
 
 mixin(ImportPhobos!());
 
-public { // required uim libraries
-  import uim.core;
-  import uim.oop;
-  import uim.apps;
+// Dub
+public import vibe.d;
+
+// UIM
+public import uim.core;
+public import uim.bootstrap;
+public import uim.html;
+public import uim.oop;
+public import uim.models;
+public import uim.apps;
+public import uim.controls;
+public import uim.servers;
+
+public import langs.javascript;
+
+public {
+  import apps.bim.controllers;
+  import apps.bim.helpers;
+  import apps.bim.tests;
+  import apps.bim.views;
 }
 
-public { // admin-bim libraries
-  import apps.bim.controllers;
-  import apps.bim.views;
+DApp bimApp;
+static this() {
+  bimApp = App
+    .name("bimApp")
+    .rootPath("/apps/bim")
+    .addRoute(Route("", HTTPMethod.GET, BIMIndexPageController))
+    .addRoute(Route("/", HTTPMethod.GET, BIMIndexPageController));
 }
