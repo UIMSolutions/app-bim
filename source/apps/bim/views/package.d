34970326c97ee813cb002e615866f7a8d8b9a6c4
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-19 08:18:06 +0200

    Updates

--- source/apps/bim/views/package.d
+++ source/apps/bim/views/package.d
@@ -1,7 +1,7 @@
 module apps.bim.views;
 
 public { // Packages
-  import apps.bim.views.componens;
+  import apps.bim.views.components;
   import apps.bim.views.forms;
   import apps.bim.views.pages;
 }
